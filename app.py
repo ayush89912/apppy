c6d51ac9c36053d6f55aff216426a4e1311e7750
ayush89912 <92029654+ayush89912@users.noreply.github.com>
2025-02-14 10:26:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def home()
+  return "Curio.us AI backend is runing!"
+
+def ask_ai():
+  return"Curio.us AI says: Keep Learning!"
+
+if __name__=='__main__':
+  app.run(host='0.0.0.0', port=5000)
